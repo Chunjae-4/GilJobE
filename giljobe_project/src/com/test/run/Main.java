19fddad6f39562cb1a607fc3e776faa78bd79673
jihyuck <wlgur4772@gmail.com>
2025-05-07 17:54:22 +0900

    jh_4번째 메인 메세지

--- giljobe_project/src/com/test/run/Main.java
+++ giljobe_project/src/com/test/run/Main.java
@@ -5,5 +5,6 @@ public class Main {
 		System.out.println("메인에 프린트메세지 추가");
 		System.out.println("메인에 프린트메세지 추가222");
 		System.out.println("메인에 프린트메세지 추가33333");
+		System.out.println("메인에 프린트메세지 추가4444444443");
 	}
 }
