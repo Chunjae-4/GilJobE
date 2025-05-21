23a4f5cb87aa024096f8e931d9ac16b81c7753f2
kkj <48651886+AAgami@users.noreply.github.com>
2025-05-21 11:30:51 +0900

    add:constants, filter & modify: mainServlet

---
+++ src/main/java/com/giljobe/common/Constants.java
@@ -0,0 +1,15 @@
+package com.giljobe.common;
+
+public class Constants {
+    //인코딩, 콘텐츠 타입
+    public static final String ENCODING = "UTF-8";
+    public static final String CONTENT_TYPE_HTML = "text/html; charset=UTF-8";
+    public static final String CONTENT_TYPE_JSON = "application/json; charset=UTF-8";
+
+    //View 경로
+    public static final String WEB_VIEWS = "/WEB-INF/views/";
+
+    //파일 업로드 경로
+    public static final String DEFAULT_UPLOAD_PATH = "/resources//upload/";
+
+}
