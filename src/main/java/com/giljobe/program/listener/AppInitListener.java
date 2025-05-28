01c0a5c7afe93dacb122b5fcf99281a2d3ab305e
kkj <48651886+AAgami@users.noreply.github.com>
2025-05-28 14:41:26 +0900

    modify: 위치 이동

---
+++ src/main/java/com/giljobe/program/listener/AppInitListener.java
@@ -0,0 +1,43 @@
+package com.giljobe.program.listener;
+
+import com.giljobe.common.LoggerUtil;
+import com.giljobe.program.model.dto.Program;
+import com.giljobe.program.model.service.ProgramService;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebListener
+public class AppInitListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        LoggerUtil.start("AppInitListener Start");
+       try{
+           List<Program> randomRecommendProgramList = new ArrayList<>();
+
+           for(int i = 1; i <= 3; i++) {
+               Program p = null;
+               p = ProgramService.getInstance().selectProgramByNo(i);
+               if (p != null) {
+                   randomRecommendProgramList.add(p);
+               }
+           }
+           ServletContext context = sce.getServletContext();
+           context.setAttribute("randomRecommend", randomRecommendProgramList);
+           LoggerUtil.debug("AppInitListener Ready:" + !randomRecommendProgramList.isEmpty());
+       } catch (Exception e){
+           LoggerUtil.error("AppInitListener Exception: "+e.getMessage(), e);
+       }
+        LoggerUtil.end("AppInitListener Init");
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        LoggerUtil.status("AppInitListener contextDestroyed:");
+    }
+}
